02b039cc13556037fdeb462c90cc20dc1c3ffa62
Steven <locke9@gmail.com>
2018-11-27 16:45:38 -0800

    Update to embree 3.3

--- embree-sys/generate_bindings.sh
+++ embree-sys/generate_bindings.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # --generate-inline-functions
+EMBREE_DIR="C:\Program Files\Intel\Embree3 x64"
 CRT="C:\Program Files (x86)\Windows Kits\10\Include\10.0.10240.0\ucrt"
 VC="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\include"
-bindgen embree3/include/embree3/rtcore.h -o pregenerated_bindings.rs \
-    --no-layout-tests --rust-target 1.25 \
+bindgen "$EMBREE_DIR\include\embree3\rtcore.h" -o pregenerated_bindings.rs \
+    --no-layout-tests \
     --whitelist-function "rtc.*" \
     --whitelist-type "RTC.*" \
     --whitelist-var "rtc.*" \
